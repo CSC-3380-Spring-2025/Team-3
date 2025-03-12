10b4a071c53264fdc28dd5683c53e0a713363906
Alexis Harvey <alexis@harveyfamilia.com>
2025-03-12 14:49:04 -0500

    Login Page Updates and Profile Page Skeleton created

--- mist/src/app/COMPONENTS/login.tsx
+++ mist/src/app/COMPONENTS/login.tsx
@@ -1,3 +1,4 @@
 /*login page for users to create a profile
 * Components to create:
 *   email, username, password, area to select if a developer or a game user
+*/
---
+++ mist/src/app/COMPONENTS/profile.tsx
@@ -0,0 +1,10 @@
+/* Profile Page
+*
+* Components: 
+* Profile Bio
+* Games user has created
+* Game stats
+* 
+* Do we want to integrate the stats page to be on the profile? 
+* 
+*/
