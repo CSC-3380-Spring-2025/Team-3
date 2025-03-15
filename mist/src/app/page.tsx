b3b94a9a40136c3b98b47ace5b80982deb6c170a
+ <+>
2025-03-15 16:53:46 -0500

    updating app/pagetsx to match HomePage name

--- mist/src/app/page.tsx
+++ mist/src/app/page.tsx
@@ -1,11 +1,7 @@
 import Link from "next/link";
 
-<<<<<<< Updated upstream
-export default function HomePage() {
-=======
 
-export default function Home() {
->>>>>>> Stashed changes
+export default function HomePage() {
   return (
     <div className="app-container min-h-screen w-full flex flex-col">
       {/* Header */}
