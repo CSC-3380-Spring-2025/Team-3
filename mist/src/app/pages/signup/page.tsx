83a73dd32e34e9840ca9726416142d82e170d07f
+ <+>
2025-03-15 17:04:54 -0500

    fixing signup page layout

--- mist/src/app/pages/signup/page.tsx
+++ mist/src/app/pages/signup/page.tsx
@@ -5,7 +5,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function Login() {
   const [email, setEmail] = useState("");
