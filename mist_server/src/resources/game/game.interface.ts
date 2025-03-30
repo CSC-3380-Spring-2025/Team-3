5e03ea770e28805a199015a71eaf99b931ee585f
+ <+>
2025-03-29 23:03:18 -0500

    updating game resources to more accuratley match our games

--- mist_server/src/resources/game/game.interface.ts
+++ mist_server/src/resources/game/game.interface.ts
@@ -2,13 +2,7 @@ import { Document } from "mongoose";
 
 export default interface Game extends Document 
 {
-    title: string; 
-    player: string;
-    sprite: string;
-    gameObject: string;
-    obstacle: string;
-    border: string;
-    enemy: string;
-    bullets: string;
-    background: string;
+    title: string;
+    gameType: string; //tetris, memory, ..
+    data: any;
 }
--- mist_server/src/resources/game/game.service.ts
+++ mist_server/src/resources/game/game.service.ts
@@ -1,28 +1,22 @@
-import GameModel from '@/resources/game/game.model'; 
+import GameModel from '@/resources/game/game.model';
 import Game from '@/resources/game/game.interface';
 
-class GameService{
-    private game = GameModel;
+class GameService {
+  private game = GameModel;
 
-    public async createGame(data: {
-      title: string;
-      sprite: string;
-      player: string;
-      gameObject?: string;
-      obstacle?: string;
-      border?: string;
-      enemy?: string;
-      bullets?: string;
-      background?: string;
-    }): Promise<Game> {
-      try {
-        const game = await this.game.create(data);
-        return game;
-      } catch (error: any) {
-        throw new Error('Unable to create game: ' + error.message);
-      }
+  public async createGame(data: {
+    title: string;
+    gameType: string;
+    data: any;
+  }): Promise<Game> {
+    try {
+      const game = await this.game.create(data);
+      return game;
+    } catch (error: any) {
+      throw new Error('Unable to create game: ' + error.message);
     }
   }
-  
+
+}
 
 export default GameService;
