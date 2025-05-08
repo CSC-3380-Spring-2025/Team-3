5c3014c9152b77d96f328f5a353c189c88ae55b9
JulianM08 <jmela41@lsu.edu>
2025-05-07 21:20:01 -0500

    correcting pull request error with development branch

--- mist/src/app/types/index.ts
+++ mist/src/app/types/index.ts
@@ -11,3 +11,10 @@ export type Game = {
     release_date: string
     freetogame_profile_url: string
 }
+
+/*
+This is mainly for the reason of creating a pull request to fix the error I caused when pushing changes I made in the development
+branch instead of making those changes in a separate branch and pulling to development. I added and edited code to make sure we could
+log in to our accounts after making them without using API requests. We instead wanted to use simple fetch requests and were able to
+implement that.
+*/
